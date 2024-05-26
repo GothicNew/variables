281a010c1888e6a6c9a6f0d83f79a7a42193a600
Gothic <kalcifer_88@mail.ru>
2024-05-26 22:38:09 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,9 @@
+#Задание переменные:
+
+homeworks_done = 12
+number_of_hours = 1.5
+course_name = "Python"
+time_per_task = number_of_hours / homeworks_done
+print('Курс:', course_name+', ' 'всего задач:', str(homeworks_done)+', ''затрачено часов:', str(number_of_hours)+', ' 'среднее время выполнения:', str(time_per_task), 'часа.')
+
+#Курс: Python, всего задач: 12, затрачено часов: 1.5, среднее время выполнения 0,125 часа.
